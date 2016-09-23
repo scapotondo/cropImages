703be924d959dc0c95639a3e9dc14482eb114a59
Sebastian Capotondo <sebastian.capotondo@gmail.com>
2016-09-23 17:56:51 -0300

    -

---
+++ src/ImageBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ImageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ImageBundle:Default:index.html.twig');
+    }
+}
---
+++ src/ImageBundle/Entity/Imagen.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ImageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Imagen
+ *
+ * @ORM\Table(name="imagen")
+ * @ORM\Entity(repositoryClass="ImageBundle\Repository\ImagenRepository")
+ */
+class Imagen
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=100)
+     */
+    private $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Imagen
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
