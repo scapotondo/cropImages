da6d79ebfc761f661b62fae82076cfe4915a10db
Sebastian Capotondo <sebastian.capotondo@gmail.com>
2016-09-24 11:30:48 -0300

    -

--- src/ImageBundle/Controller/DefaultController.php
+++ src/ImageBundle/Controller/DefaultController.php
@@ -4,14 +4,38 @@ namespace ImageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/upload", name="upload")
+     * @Template()
      */
     public function indexAction()
     {
+
         return $this->render('ImageBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="create_imagen")
+     */
+    public function createAction(Request $request){
+        $imagen = $this->get('subirImagen');
+        var_dump($request->request->all());
+        $imagenes = $imagen->getImages();
+
+        if ($imagenes == false) {
+            var_dump('mal'); exit;
+        }else{
+            foreach ($imagenes as $imagen) {
+                $this->get('subirImagen')->saveFile($imagen['output']['data'], $imagen['input']['name'], '');
+            }
+        }
+
+        return $this->redirect($this->generateUrl('upload'));
+
     }
 }
